ec22ff8e2aeb63f7b4d194c44d3cb659a39c2231
Derik <dm554@njit.edu>
2020-10-20 18:15:51 -0400

    Registration Functional

--- SQL/sql_connect.py
+++ SQL/sql_connect.py
@@ -1,34 +1,36 @@
 import mysql.connector, pika, sys, os
+    
+
 
 def main():
-    user_name;
-    pass_word;
+
     
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
 
     channel.queue_declare(queue='user_key')
-    channel.queue_declare(queue='pass_key'
+    channel.queue_declare(queue='pass_key')
     
     
     def callback1(ch, method, properties, body):
         print(" [x] Received %r" % body)
-        user_name = body
+        info = body.split()
+        sql_query(info[0], info[1])
+        
         
     def callback2(ch, method, properties, body):
         print(" [x] Received %r" % body)
         pass_word = body
-    
-    
-        
+     
     
     channel.basic_consume(queue='user_key', on_message_callback=callback1, auto_ack=True)
     channel.basic_consume(queue='pass_key', on_message_callback=callback2, auto_ack=True)
-    sql_query(user_name, pass_word)
+    #sql_query(user_name, pass_word)
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
+    #sql_query(user_name, pass_word)
 
-def sql_query(String uname, String pword):  
+def sql_query(uname, pword):  
 	cnx = mysql.connector.connect(
 		host = "localhost",
 		user = "test_user",
@@ -40,7 +42,7 @@ def sql_query(String uname, String pword):
 	query = ("INSERT INTO accounts (email, password) VALUES(%s, %s);")
 	mycursor = cnx.cursor()
 	print("Cursor made")
-	mycursor.execute(query, (email, data_password))
+	mycursor.execute(query, (uname, pword))
 	cnx.commit()
 	cnx.close()
 	
---
+++ flask_web/page.py
@@ -0,0 +1,42 @@
+from flask import Flask, redirect, url_for, render_template, request
+import pika
+
+app = Flask(__name__)
+
+#@app.route("/")
+#def home():
+#    return render_template("index.html")
+
+@app.route("/register", methods=["POST", "GET"])
+def register():
+    connection = pika.BlockingConnection( pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+    channel.queue_declare(queue='user_key')
+    channel.queue_declare(queue='pass_key')
+    if request.method == "POST":
+       if request.form.get("login"):
+          return redirect(url_for("login")) 
+       else:
+          user = request.form["username"]
+          password = request.form["password"]
+          info = user + ' ' + password
+          channel.basic_publish(exchange='', routing_key='user_key', body=info)
+          print(user)
+          channel.basic_publish(exchange='', routing_key='pass_key', body=password)
+          connection.close()
+          return redirect(url_for("user", usr=user))
+    elif request.method == "GET":
+          return render_template("index.html")
+    
+
+@app.route("/login")
+def login():
+    return render_template("login.html") 
+    
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
