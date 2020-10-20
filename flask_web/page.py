2a9e45cea85aac8967a7f914ca805976e0c7ac2b
zaheen <aa2552@njit.edu>
2020-10-21 07:16:39 +0800

    Login + Registration Frontend + queue-ing to RabbitMQ done

--- flask_web/page.py
+++ flask_web/page.py
@@ -7,37 +7,49 @@ app = Flask(__name__)
 #def home():
 #    return render_template("index.html")
 
-@app.route("/login", methods=["POST", "GET"])
-def login():
+@app.route("/register", methods=["POST", "GET"])
+def register():
     connection = pika.BlockingConnection( pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
     channel.queue_declare(queue='user_key')
     channel.queue_declare(queue='pass_key')
     if request.method == "POST":
-       if request.form.get("register"):
-          return redirect(url_for("register")) 
+       if request.form.get("login"):
+          return redirect(url_for("login")) 
        else:
           user = request.form["username"]
           password = request.form["password"]
-          info = user + password
+          info = user + ' ' + password
           channel.basic_publish(exchange='', routing_key='user_key', body=info)
-          print(user)
-         # channel.basic_publish(exchange='', routing_key='pass_key', body=password)
+          #print(user)
+          channel.basic_publish(exchange='', routing_key='pass_key', body=password)
           connection.close()
           return redirect(url_for("user", usr=user))
     elif request.method == "GET":
           return render_template("index.html")
     
 
-@app.route("/register")
-def register():
-    return render_template("register.html") 
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    connection = pika.BlockingConnection( pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+    if request.method == "POST":
+       if request.form.get("register"):
+          return redirect(url_for("register")) 
+       else:
+          user = request.form["username"]
+          password = request.form["password"]
+          info = user + ' ' + password
+          channel.basic_publish(exchange='', routing_key='user_key', body=info)
+          connection.close()
+          return redirect(url_for("user", usr=user))
+    elif request.method == "GET":
+          return render_template("login.html")
     
 @app.route("/<usr>")
 def user(usr):
-    return f"<h1>{usr}</h1>"
+    return f"<h1>Logged In: {usr}</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
     
-
