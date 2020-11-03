ab5b5edb2667f42d28d3bef040f90c0651ed1174
Derik <dm554@njit.edu>
2020-11-02 21:26:43 -0500

    Login confirmation

--- SQL/sql_connect.py
+++ SQL/sql_connect.py
@@ -10,13 +10,14 @@ def main():
 
     channel.queue_declare(queue='user_key')
     channel.queue_declare(queue='pass_key')
+    channel.queue_declare(queue='access')
     
     
     def callback1(ch, method, properties, body):
         print(" [x] Received %r" % body)
         b = body.decode('utf-8')
         info = b.split()
-        sql_query(info[0], info[1], info[2])
+        sql_query(info[0], info[1], info[2], channel)
         
         
     def callback2(ch, method, properties, body):
@@ -31,10 +32,10 @@ def main():
     channel.start_consuming()
     #sql_query(user_name, pass_word)
 
-def sql_query(opt, uname, pword):  
+def sql_query(opt, uname, pword, channel):  
 	cnx = mysql.connector.connect(
 		host = "localhost",
-		user = "test_user",
+		user = "backend",
 		password = "password",
 		database = "db_test"
 	)
@@ -50,9 +51,15 @@ def sql_query(opt, uname, pword):
 		query = ("INSERT INTO accounts (email, password) VALUES(%s, %s);")
 		mycursor.execute(query, (uname, pword))
 	elif option == 'login':
-		query = ("SELECT FROM accounts WHERE (email, password) VALUES(%s,%s);")
-		mycursor.execute(query, (uname, pword))
+		query = ("SELECT * FROM accounts WHERE email='%s' AND password='%s';" % (uname, pword))
+		mycursor.execute(query)
 		myresult = mycursor.fetchall()
+		print(len(myresult))
+		if len(myresult) > 0:
+			print("logged")
+			channel.basic_publish(exchange='',routing_key='access',body='logged')
+		else:
+			channel.basic_publish(exchange='',routing_key='access',body='invalid')
 	else:
 		print("Didnt work")
 	
